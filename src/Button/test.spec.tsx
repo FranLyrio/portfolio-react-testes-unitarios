e953cfd3ade674119ede222ff8cdcbafe55e84b5
FranLyrio <lyriofranciele@gmail.com>
2022-01-29 16:53:47 -0300

    test(button): testa tamanho, espaçamento e cor de fundo

--- src/Button/test.spec.tsx
+++ src/Button/test.spec.tsx
@@ -4,10 +4,14 @@ import { Button } from '.'
 
 describe('<Button />', () => {
   it('Should render button component by default', () => {
-    render(<Button />)
+    render(<Button>Click me</Button>)
 
     expect(
       screen.getByRole('button', { name: /click me/i })
-    ).toBeInTheDocument()
+    ).toHaveStyle({
+      height: '3rem',
+      padding: '0 1.25rem',
+      'background-color': '#820ad1'
+    })
   })
 })
