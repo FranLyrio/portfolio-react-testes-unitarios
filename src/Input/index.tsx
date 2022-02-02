998ba4d9ab208260a4f507df23a39c6fd12c7753
FranLyrio <lyriofranciele@gmail.com>
2022-02-02 19:55:37 -0300

    feat(input): adiciona propriedades

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -1,8 +1,15 @@
-export const Input = () => {
+import { InputHTMLAttributes } from "react"
+
+export type InputProps = {
+  label?: string;
+} & InputHTMLAttributes<HTMLInputElement>
+
+export const Input = ({ name, label, ...props }: InputProps) => {
   return (
     <>
-      <label></label>
-      <input />
+      {!!label && <label htmlFor={name}>{label}</label>}
+      
+      <input id={name} {...props} />
     </>
   )
 }
