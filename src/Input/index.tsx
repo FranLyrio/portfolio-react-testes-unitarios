51b83a3a8635664010544cca1caf9042eba3e74c
Franciele Lyrio <fran@MacBook-Air-de-Franciele.local>
2022-02-05 16:56:00 -0300

    feat(Input): adiciona possibilidade de utilizar componente com ícone

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -1,15 +1,20 @@
-import { InputHTMLAttributes } from "react"
+import { InputHTMLAttributes, ReactNode } from "react"
+
+import * as S from './styles'
 
 export type InputProps = {
   label?: string;
+  icon?: ReactNode;
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const Input = ({ name, label, ...props }: InputProps) => {
+export const Input = ({ name, label, icon, ...props }: InputProps) => {
   return (
-    <>
+    <S.Wrapper>
       {!!label && <label htmlFor={name}>{label}</label>}
+
+      {!!icon && icon}
       
       <input id={name} {...props} />
-    </>
+    </S.Wrapper>
   )
 }
---
+++ src/Input/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  border: 0.2rem solid;
+  border-color: #fff;
+
+  &:focus-within {
+    border-color: #820ad1;
+  }
+`
